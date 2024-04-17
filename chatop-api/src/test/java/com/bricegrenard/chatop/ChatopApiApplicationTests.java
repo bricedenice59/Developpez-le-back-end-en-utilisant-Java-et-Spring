7fd1f5bfe6fe6f9c278dbb86361e6266eeaf7530
Brice Grenard <brice.grenard@gmail.com>
2024-04-17 07:38:06 +0200

    Creation of an empty Spring solution.

---
+++ chatop-api/src/test/java/com/bricegrenard/chatop/ChatopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bricegrenard.chatop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
