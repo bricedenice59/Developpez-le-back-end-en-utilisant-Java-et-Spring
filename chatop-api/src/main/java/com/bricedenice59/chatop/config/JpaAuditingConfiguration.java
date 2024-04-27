6dbe9005ab2736fee0ae5685b28ad031e5046b9f
Brice Grenard <brice.grenard@gmail.com>
2024-04-27 04:01:42 +0200

    Attempt to get the springboot project working inside a container...

---
+++ chatop-api/src/main/java/com/bricedenice59/chatop/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.bricedenice59.chatop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
